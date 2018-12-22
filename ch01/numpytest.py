00407c915ce7e02186587ce36830f67e1d6835ab
lockmachine <34362144+lockmachine@users.noreply.github.com>
2018-12-23 00:01:47 +0900

    よくわからなくなったので、いったんpullしてpushする

--- ch01/numpytest.py
+++ ch01/numpytest.py
@@ -27,19 +27,3 @@ print(x / y)    # 要素ごとの割り算
 
 print("x / 2.0:")
 print(x / 2.0)	# 各要素を2で割る
-
-# NumpyのN次元配列
-print("NumpyのN次元配列")
-A = np.array([[1, 2],[3, 4]])
-print(A)
-
-print(A.shape)
-print(A.dtype)
-
-B = np.array([[3, 0],[0, 6]])
-print(B)
-print(A + B)
-print(A * B)
-
-print(A)
-print(A * 10)
