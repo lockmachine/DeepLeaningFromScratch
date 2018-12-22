7c32becfd8ebb65241e2884559f15f9110767298
lockmachine <34362144+lockmachine@users.noreply.github.com>
2018-12-23 07:38:43 +0900

    Numpyの写経終了

--- ch01/numpytest.py
+++ ch01/numpytest.py
@@ -28,4 +28,39 @@ print(x / y)    # 要素ごとの割り算
 print("x / 2.0:")
 print(x / 2.0)	# 各要素を2で割る
 
-# NumPyのN次元配列
+
+print("\n# 1.5.4 NumPyのN次元配列")
+A = np.array([[1, 2], [3, 4]])
+print(A)
+
+print(A.shape)
+print(A.dtype)
+
+B = np.array([[3, 0], [0, 6]])
+print(A + B)
+
+print(A * B)
+
+print(A * 10)
+
+print("\n# 1.5.5 ブロードキャスト")
+B = np.array([10, 20])
+print(A * B)	# [[10, 40], [30, 80]]
+
+print("\n# 1.5.6 要素へのアクセス")
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print(X)
+print(X[0])
+print(X[0][1])
+print(X[:,1])
+
+print("for文")
+for row in X:
+	print(row)
+
+X = X.flatten()
+print(X)	# X.shapeは（6, )
+print(X[np.array([0, 2, 4])])	#インデックスが０，２，４番目の要素を取得
+
+print(X > 15)
+print(X[X >15])
