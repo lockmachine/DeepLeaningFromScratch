388aea1e39ce6d8aaf0e2243d620069386253f1a
lockmachine <34362144+lockmachine@users.noreply.github.com>
2018-12-24 07:42:17 +0900

    重みとバイアスおよび多層パーセプトロンの実装

--- ch02/perceptron.py
+++ ch02/perceptron.py
@@ -1,5 +1,6 @@
 print("\n# 2.3.1 簡単な実装")
 
+'''# 2.3.3 のためにコメントアウト
 def AND(x1, x2):
 	w1, w2, theta = 0.5, 0.5, 0.7
 	tmp = x1*w1 + x2*w2
@@ -12,6 +13,7 @@ print(AND(0, 0))	# 0 を出力
 print(AND(0, 1))	# 0 を出力
 print(AND(1, 0))	# 0 を出力
 print(AND(1, 1))	# 1 を出力
+'''
 
 print("\n# 2.3.2 重みとバイアスの導入")
 import numpy as np
@@ -19,6 +21,52 @@ x = np.array([0, 1])		# 入力
 w = np.array([0.5, 0.5])	# 重み
 b = -0.7					# バイアス
 
-print(w*x)
-print(np.sum(w*x))
-print(np.sum(w*x) + b)
+print(w*x)					# 入力x重み
+print(np.sum(w*x))			# 入力x重みの総和
+print(np.sum(w*x) + b)		# 入力x重みの総和にバイアスをかける
+
+print("\n# 2.3.3 重みとバイアスによる実装")
+
+def AND(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
+	b = -0.7
+	tmp = np.sum(x*w) + b
+	if tmp <= 0:
+		return 0
+	elif tmp > 0:
+		return 1
+
+
+def NAND(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([-0.5, -0.5])
+	b = 0.7
+	tmp = np.sum(x*w) + b
+	if tmp <= 0:
+		return 0
+	elif tmp > 0:
+		return 1
+
+
+def OR(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
+	b = -0.3
+	tmp = np.sum(w*x) + b
+	if tmp <= 0:
+		return 0
+	elif tmp > 0:
+		return 1
+
+print("\n# 2.5.2 XORゲートの実装")
+def XOR(x1, x2):
+	y_nand = NAND(x1, x2)
+	y_or = OR(x1, x2)
+	y = AND(y_nand, y_or)
+	return y
+
+print(XOR(0, 0))	# 0 を
+print(XOR(0, 1))	# 1 を
+print(XOR(1, 0))	# 1 を
+print(XOR(1, 1))	# 0 を
