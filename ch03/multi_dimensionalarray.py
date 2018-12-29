77eac549b38962a6c59e5a217a020fab840cf46c
lockmachine <34362144+lockmachine@users.noreply.github.com>
2018-12-29 13:58:34 +0900

    多次元配列の写経開始

---
+++ ch03/multi_dimensionalarray.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("\n#3.3.1 多次元配列")
+A = np.array([1, 2, 3, 4])
+print(A)
+print(np.ndim(A))
+print(A.shape)
+print(A.shape[0])
+
+B = np.array([[1,2], [3,4], [5,6]])
+print(B)
+print(np.ndim(B))
+print(B.shape)
+print(B.shape[0])
