089aaad4591149ddb9eb7cedb7e182fe1786f66b
lockmachine <34362144+lockmachine@users.noreply.github.com>
2018-12-30 12:55:49 +0900

    3層ニューラルネットワークの実装の写経終了

--- ch03/3layer_nn.py
+++ ch03/3layer_nn.py
@@ -1,4 +1,42 @@
 import numpy as np
+import sigmoid as sig
+import identity_function as identity
 
 print("\n#3.4 3層ニューラルネットワークの実装")
 print("\n#3.4.2 各層における信号伝達の実装")
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+print("X = ")
+print(X)
+print("W1 = ")
+print(W1)
+print("B1 = ")
+print(B1)
+
+print("A1 = X * W1 + B1")
+A1 = np.dot(X, W1) + B1
+print(A1)
+
+print("Aに活性化関数としてシグモイド関数を適用する")
+Z1 = sig.sigmoid(A1)
+print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+print("A2 = Z1 * W2 + B2")
+A2 = np.dot(Z1, W2) + B2
+Z2 = sig.sigmoid(A2)
+print("Z2")
+print(Z2)
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+print("A3 = Z2 * W3 + B3")
+A3 = np.dot(Z2, W3) + B3
+Z3 = identity.identity_function(A3)
+print("Z3")
+print(Z3)
---
+++ ch03/sigmoid.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+if __name__ == "__main__":
+    pass
