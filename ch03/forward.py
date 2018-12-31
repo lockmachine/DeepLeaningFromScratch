a09a5c6c0832c84ac3ceb63c702a6f4fffb1ea36
lockmachine <34362144+lockmachine@users.noreply.github.com>
2018-12-31 11:58:58 +0900

    3.4.3 実装のまとめの章の写経終了

---
+++ ch03/forward.py
@@ -0,0 +1,38 @@
+import numpy as np
+import sigmoid as sig
+import identity_function as identity
+import pprint
+
+def init_network():
+    network = {}
+    network["W1"] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network["b1"] = np.array([0.1, 0.2, 0.3])
+    network["W2"] = np.array([[0.1, 0.4], [0.2 ,0.5], [0.3, 0.6]])
+    network["b2"] = np.array([0.1, 0.2])
+    network["W3"] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network["b3"] = np.array([0.1, 0.2])
+#    pprint.pprint(type(network))
+#    pprint.pprint(network)
+
+    return network
+
+def forward(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sig.sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sig.sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity.identity_function(a3)
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+pprint.pprint(y)
+
+if __name__ == '__main__':
+    init_network()
