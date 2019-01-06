d94d9af380b79ef1838db853a248d30101a2bae2
lockmachine <34362144+lockmachine@users.noreply.github.com>
2019-01-07 06:08:07 +0900

    cross_entropy_error

--- ch04/mean_squared_error.py
+++ ch04/mean_squared_error.py
@@ -3,4 +3,18 @@ import numpy as np
 def mean_squared_error(y, t):
 	e = 0.5 * np.sum((y-t)**2)
 	return e
-	
+
+def cross_entropy_error(y, t):
+	delta = 1e-7
+	e = -np.sum(t * np.log(y+delta))
+	return e
+
+if __name__ == "__main__":
+	t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+	y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0])
+	print("mean_squared_error : " + str(mean_squared_error(y, t)))
+	print("cross_entropy_error: " + str(cross_entropy_error(y, t)))
+
+	y = np.array([0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0])
+	print("mean_squared_error : " + str(mean_squared_error(y, t)))
+	print("cross_entropy_error: " + str(cross_entropy_error(y, t)))
