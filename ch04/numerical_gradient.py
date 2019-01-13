3da4adfe35dc4741c7edc9c5bf58d82cd3860065
lockmachine <34362144+lockmachine@users.noreply.github.com>
2019-01-13 10:56:54 +0900

    added numerical_gradient.py

---
+++ ch04/numerical_gradient.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import sys,os
+sys.path.append(os.pardir)
+import numpy as np
+import matplotlib.pyplot as plot
+
+def function_2(x):
+	if x.ndim == 1:
+		return np.sum(x**2)
+	else:
+		return np.sum(x**2, axis=1)
+
+def numerical_gradient(f, x):
+	h = 1e-4	# 0.0001
+	grad = np.zeros_like(x)	# xと同じ形状の配列を生成
+
+	for idx in range(x.size):
+		tmp_val = x[idx]
+		# f(x+h) の計算
+		x[idx] = tmp_val + h
+		fxh1 = f(x)
+
+		# f(x-h) の計算
+		x[idx] = tmp_val - h
+		fxh2 = f(x)
+
+		grad[idx] = (fxh1 - fxh2) / (2*h)
+		x[idx] = tmp_val
+
+	return grad
+
+if __name__ == "__main__":
+	a = numerical_gradient(function_2, np.array([3.0, 4.0]))
+	print("numerical_gradient(function_2, np.array([3.0, 4.0]) = " + str(a))
+	b = numerical_gradient(function_2, np.array([0.0, 2.0]))
+	print("numerical_gradient(function_2, np.array([0.0, 2.0]) = " + str(b))
+	c = numerical_gradient(function_2, np.array([3.0, 0.0]))
+	print("numerical_gradient(function_2, np.array([3.0, 0.0]) = " + str(c))
