fc39a73df42c0d2a489e3af1c8de11c77f3c0c6f
lockmachine <34362144+lockmachine@users.noreply.github.com>
2019-01-27 11:27:03 +0900

    changed my_layers.py

--- ch05/my_layers.py
+++ ch05/my_layers.py
@@ -30,6 +30,35 @@ class mySigmoid:
     def backward(self, dout):
         dx = dout * self.out * (1.0 - self.out)
         return dx
+        
+class myAffine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.original_x_shape = None    # テンソル対応用
+        
+        # 重み・バイアスパラメータの微分
+        self.dW = None
+        self.db = None
+        
+    def forward(self, x):
+        # テンソル対応
+        self.original_x_shape = self.shape  # 元々の入力データの形状を記憶
+        x = x.reshape(x.shape[0], -1)       # 1 次元目の要素数で固定した行列形状にする
+        
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        
+        return out
+        
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        
+        dx = dx.reshape(*self.original_x_shape) # 入力データの形状を元に戻す（テンソル対応）
+        return dx
     
 def main():
     pass
