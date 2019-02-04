2d46dfc24dbc744de87e8535885064f7152d2742
lockmachine <34362144+lockmachine@users.noreply.github.com>
2019-02-05 05:36:00 +0900

    changed my_optimizer.py for add Momentum

--- ch06/my_optimizer.py
+++ ch06/my_optimizer.py
@@ -10,3 +10,19 @@ class SGD:
         for key in params.keys():
             params[key] -= self.lr * grads[key]
             
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        
+    def update(self, params, grads):
+        # 最初に呼ばれた場合は v は params と同じ構造のデータをディクショナリ変数として保持する
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_lize(val)
+                
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
