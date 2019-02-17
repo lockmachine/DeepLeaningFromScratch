8cbdac78325b7accbc5fabe6d1f579ecaac127e1
lockmachine <34362144+lockmachine@users.noreply.github.com>
2019-02-17 20:44:04 +0900

    changed my_Convolution.py for underconstruction

--- ch07/my_Convolution.py
+++ ch07/my_Convolution.py
@@ -7,12 +7,21 @@ import matplotlib.pyplot as plt
 from common.util import im2col, col2im
 
 
-class Convolution:
+class myConvolution:
     def __init__(self, W, b, stride=1, pad=0):
         self.W = W
         self.b = b
         self.stride = stride
         self.pad = pad
+        
+        # backward 時に使用
+        self.x = None
+        self.col = None
+        self.col_W = None
+        
+        # 重み・バイアスの勾配
+        self.dW = None
+        self.db = None
         
     def forward(self, x):
         # フィルターのサイズ取得
@@ -27,8 +36,23 @@ class Convolution:
         
         col = im2col(x, FH, FW, self.stride, self.pad)  # (OH*OW)x(C*FW*FH)
         col_W = self.W.reshape(FN, -1).T    # フィルターの展開 FNx(FW*FH*C)
-        out = np.dot(col, col_W) + self.b
         
+        out = np.dot(col, col_W) + self.b
         out = out.reshape(N, OH, OW, -1).transpose(0, 3, 1, 2)
         
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+        
         return out
+        
+    def backward(self, dout):
+        # フィルターのサイズを取得
+        FN, C, FH, FW = self.W.shape
+        
+        # 逆伝播の入力データを順伝播のtranspose前の行列形式に変換
+        # forward の最終出力が out.reshape.transpose なので
+        # backwardの入力を dout.transpose.reshape にする
+        dout = dout.transpose(0, 2, 3, 1).reshape(-1, FN)
+        
+        db = 
