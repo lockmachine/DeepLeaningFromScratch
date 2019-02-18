f503ecb08e8ad603a143f7a4ce88474c5103d135
lockmachine <34362144+lockmachine@users.noreply.github.com>
2019-02-19 01:04:57 +0900

    changed my_Convolution.py for underconstruction(in Pooling)

--- ch07/my_Convolution.py
+++ ch07/my_Convolution.py
@@ -59,8 +59,47 @@ class myConvolution:
         順伝播でのバイアス加算は、それぞれのデータ（1個目のデータ、2個目のデータ、・・・）に対して加算が行われる。
         そのため、逆伝播の際には、それぞれのデータの逆伝播の値がバイアスの要素に集約される必要がある。
         """
-        # db = dL/dB = dL/dY = dout で、1x1xFNの形にする
+        # db = dL/dB = dL/dY = dout となるので、1x1xFNの形にする
         self.db = np.sum(dout, axis=0)
+        
+        # dL/dW = X.T * dL/dY となるので、
         self.dW = np.dot(self.col.T, dout)
+        self.dW = (self.T).reshape(FN, C, FH, FW)
         
+        # dL/dx = dL/dY * W.T
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad )
         
+        return dx
+
+class myPooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+        
+    def forward(self, x):
+        N, C, H, W = x.shape
+        self.x = x
+        OH = int(1 + (H - self.pool_h) / self.stride)
+        OW = int(1 + (W - self.pool_w) / self.stride)
+        
+        # 展開（１）
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        # col は (N x OH x OW, C x pool_h x pool_w) になっているので、チャンネル毎に縦に並べる
+        # (N x OH x OW x C, pool_h x pool_w) となる
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+        
+        # 最大値（２）
+        # 各プーリング領域に対して最大値を求める
+        # (N x OH x OW x C, 1) のサイズになる
+        out = np.max(col, axis=1)
+        
+        # 整形（３）
+        out = out.reshape(N, OH, OW, C).transpose(0, 3, 1, 2)
+        
+        return out
+        
+        def backward(self, dout):
+            
